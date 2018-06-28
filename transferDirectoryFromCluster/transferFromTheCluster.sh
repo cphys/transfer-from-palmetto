c8523d487b18771a84c9ab6082226f48b8e4b8ec
cphys <cpm2@g.clemson.edu>
2018-06-28 17:22:10 -0400

    input and output directories were reversed, removed spaces in variable assignments

--- transferDirectoryFromCluster/transferFromTheCluster.sh
+++ transferDirectoryFromCluster/transferFromTheCluster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-clusterUserName = "cpm2"
-directoryOnCluster = /dataFull/
-fullClusterAddress = $clusterUserName"@xfer01-ext.palmetto.clemson.edu:${PWD/$USER/$clusterUserName}"$directoryOnCluster
-#echo $fullClusterAddress
 
-scp -r . $fullClusterAddress
+clusterUserName="cpm2"
+directoryOnCluster=/dataFull/
+fullClusterAddress=$clusterUserName"@xfer01-ext.palmetto.clemson.edu:${PWD/$USER/$clusterUserName}"$directoryOnCluster
+
+
+scp -r $fullClusterAddress .
